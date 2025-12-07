b95f8f127523d78409e12d3b4fa32743666b30e6
leeladhar248029-ctrl <leeladhar248029@gmail.com>
2025-12-07 13:36:39 +0530

    day 1

---
+++ if-else.py
@@ -0,0 +1,13 @@
+license = False
+drunk = True
+
+if license:
+    if drunk:
+        print(1000)
+    else:
+        print(0)
+else:
+    if drunk:
+        print(3000)
+    else:
+        print(2000)
